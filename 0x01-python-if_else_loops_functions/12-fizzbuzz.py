f9b7befb20ad2e25a208597912b073d55a75a715
Liam Bowers <liambowers718@gmail.com>
2023-08-08 11:49:38 -0700

    Converts to uppercase letters

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     s = ""
-    for i in range(1,101):
+    for i in range(1, 101):
         if i % 3 == 0:
             s = "Fizz"
             if i % 5 == 0:
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * insert_node - inserts a new number in list
+ * @head: pointer to pointer of head
+ * @number: value to be added
+ * Return: Null on faliure; Pointer to new element on sucess
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node = NULL;
+	listint_t *curr = NULL;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	curr = *head;
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = number;
+	if (curr->n > number)
+	{
+		new_node->next = curr;
+		*head = new_node;
+		return (new_node);
+	}
+	while (curr->next != NULL)
+	{
+		if ((curr->next->n) > number)
+		{
+			new_node->next = curr->next;
+			curr->next = new_node;
+			return (new_node);
+		}
+		curr = curr->next;
+	}
+	curr->next = new_node;
+	new_node->next = NULL;
+	return (new_node);
+}
