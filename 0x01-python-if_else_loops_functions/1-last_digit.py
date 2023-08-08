43a54e7aaa142fff90d84bc60e04004858afea66
Liam Bowers <liambowers718@gmail.com>
2023-08-08 06:09:45 -0700

    Determine wether number is positive or negative

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = str(number)[-1]
+s = ""
+if number < 0:
+    last = "-" + last
+if int(last) > 5:
+    s += "and is greater than 5"
+elif int(last) == 0:
+    s += "and is 0"
+elif int(last) < 6 and int(last) != 0:
+    s += "and is less than 6 and not 0"
+print("The last digit of {} is {} {}".format(number, last, s))
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,3 @@
+0 - Determine if number is positive or negative
+1 - Print last digit of number
+2 - Print all letters of the alphabet
