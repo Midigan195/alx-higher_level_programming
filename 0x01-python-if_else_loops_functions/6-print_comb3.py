a2d775eefcefd6cad1b86db6b08db91c50a85465
Liam Bowers <liambowers718@gmail.com>
2023-08-09 08:00:04 -0700

    Calculate a to the power of b

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if (i == j):
+for i in range(48, 58):
+    for j in range(48, 58):
+        if i == j:
             continue
+        if (i - j) < (j - i):
+            print("{}{}".format(chr(i), chr(j)), end="")
+            if i == 56 and j == 57:
+                break
+            print(", ", end="")
+print()
