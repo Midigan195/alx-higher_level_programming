0f6131a29c0a343963edeb1fb207df0cbf2be698
Liam Bowers <liambowers718@gmail.com>
2023-08-28 05:36:26 -0700

    Print list with exception handling

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    number = ""
+    if x <= 0 or not my_list:
+        return 0
+    for i in range(0, x):
+        try:
+            number = number + str(my_list[i])
+        except IndexError:
+            i -= 1
+            break
+    print("{}" .format(number))
+    return i + 1
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+0 - Prints elements of a list
