0bbc49831df56ee71166957f518606c8f83326bb
Liam Bowers <liambowers718@gmail.com>
2023-08-29 12:11:37 -0700

    Define a class with attribute validation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -97,7 +97,8 @@ class Square:
         """
         if self.__size == 0:
             print()
-        for i in range(0, self.__position[1]):
-            print()
+        if not self.__position[1] > 0:
+            for i in range(0, self.__position[1]):
+                print()
         for i in range(0, self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
