6cb47df0ed74df8d936e77f9359a7450e871b66a
Liam Bowers <liambowers718@gmail.com>
2023-11-13 05:16:08 -0800

    Print a variable to the terminal

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
-if (process.argv.length <= 2) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < process.argv[2]; i++) {
-    console.log('X'.repeat(process.argv[2]));
+const size = process.argv[2];
+
+if (!isNaN(parseInt(size))) {
+  const squareSize = parseInt(size);
+  for (let i = 0; i < squareSize; i++) {
+    let row = '';
+    for (let j = 0; j < squareSize; j++) {
+      row += 'X';
+    }
+    console.log(row);
   }
+} else {
+  console.log('Missing size');
 }
